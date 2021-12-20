ddd379c5eac018cef9d81778597deef559926729
magiconch <magiconch@outlook.com>
2021-12-20 23:40:32 +0800

    add new solution

--- tscode/300.最长递增子序列.ts
+++ tscode/300.最长递增子序列.ts
@@ -6,6 +6,10 @@
 
 // @lc code=start
 function lengthOfLIS(nums: number[]): number {
+    return solution300_2(nums);
+};
+
+function solution300_1(nums: number[]): number {
     const result: number[] = [];
     for (let index = 0; index < nums.length; index++) {
         result.push(1);
@@ -22,6 +26,26 @@ function lengthOfLIS(nums: number[]): number {
     return Math.max(...result);
 
 };
+
+function solution300_2(nums: number[]): number {
+    const dp: number[] = [];
+    for (let index = 0; index < nums.length; index++) {
+        dp.push(1);
+    }
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) 
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+        }
+    }
+    
+    let res = 0;
+    for (let i = 0; i < dp.length; i++) {
+        res = Math.max(res, dp[i]);
+    }
+    return res;
+};
+
 // @lc code=end
 
 lengthOfLIS([0,1,0,3,2,3]);
