cf95a5e96274f7ca561baaf0e178b583733b8c33
magiconch <magiconch@outlook.com>
2021-12-26 23:43:24 +0800

    add solution2

--- tscode/72.编辑距离.ts
+++ tscode/72.编辑距离.ts
@@ -6,7 +6,7 @@
 
 // @lc code=start
 function minDistance(word1: string, word2: string): number {
-    return soultion_72_1(word1, word2);
+    return soultion_72_2(word1, word2);
 };
 
 function soultion_72_1(word1: string, word2: string): number {
@@ -27,6 +27,38 @@ function soultion_72_1(word1: string, word2: string): number {
                 dp(i, j - 1), // 添加一个字符到word1，word2的指针往前移动
                 dp(i - 1, j - 1) // 替换当前字符，指针都往前移动
             ) + 1;
+        }
+    }
+    return dp(word1.length - 1, word2.length - 1);
+}
+
+function soultion_72_2(word1: string, word2: string): number {
+    // 我们需要明确的是，我们的编辑距离是相对于word1进行的编辑
+
+    // 备忘录
+    const memo: number[][] = [];
+    function dp(i:number, j: number): number {
+        if (i == -1) { // 表明word1已经遍历完成
+            return j + 1; // 把word2剩下的所有字符插入到word1
+        }
+        if (j == -1) { // 表明word2已经遍历完成
+            return i + 1; // 把word1剩下的所有字符删除
+        }
+
+        if (word1[i] == word2[j]) {
+            if (!memo[i] && !memo[i][j]) {
+                memo[i][j] = dp(i - 1, j - 1);
+            }
+            return memo[i][j];
+        } else {
+            if (!memo[i] && !memo[i][j]) {
+                memo[i][j] = Math.min(
+                    dp(i - 1, j), // 删除当前的字符， word1的指针往前移动
+                    dp(i, j - 1), // 添加一个字符到word1，word2的指针往前移动
+                    dp(i - 1, j - 1) // 替换当前字符，指针都往前移动
+                ) + 1;
+            }
+            return memo[i][j];
         }
     }
     return dp(word1.length - 1, word2.length - 1);
