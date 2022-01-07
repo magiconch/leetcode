642abd355e2ca6d4391a33d7ee74dff4ab95a120
magiconch <magiconch@outlook.com>
2022-01-07 20:21:05 +0800

    Fix the bug

--- tscode/53.最大子数组和.ts
+++ tscode/53.最大子数组和.ts
@@ -6,22 +6,63 @@
 
 // @lc code=start
 function maxSubArray(nums: number[]): number {
-    let result: number = nums.length > 0 ? nums[0] : -Number.MAX_VALUE;
+    return solution_53_1(nums);
+};
+
+function solution_53_1(nums: number[]): number {
+    if (nums.length < 1) {
+        return 0;
+    }
+    nums = numsFilter(nums);
+    let result: number = nums[0];
+    let allNegativeNumbers: number = 0;
     for (let i = 0; i < nums.length; i++) {
-        let tempMaxSub = -Number.MAX_VALUE;
+        if (nums[i] < 0) {
+            allNegativeNumbers++;
+            continue;
+        }
         let tempSum = nums[i];
+        let tempMaxSub = tempSum;
         for (let j = i + 1; j < nums.length; j++) {
+            tempSum += nums[j];
             if (tempSum > tempMaxSub) {
                 tempMaxSub = tempSum;
             }
-            tempSum += nums[j];
         }
         if (tempMaxSub > result) {
             result = tempMaxSub;
         }
     }
+    if (allNegativeNumbers == nums.length) {
+        return Math.max(...nums);
+    }
     return result;
 };
+/**
+ * 将所有相邻的正数和所有相邻的负数都放到一起
+ * 特殊情况，当所有的都是负数的时候。
+ * @param nums 
+ * @returns 
+ */
+function numsFilter(nums: number[]): number[] {
+    const result: number[] = [];
+    for (let i = 0; i < nums.length; i++) {
+        
+        let tempSumVal: number = nums[i];
+        let j: number = i + 1;
+        while(nums[i] * nums[j] > 0) {
+            tempSumVal += nums[j];
+            j++;
+            i++;
+        }
+        result.push(tempSumVal);
+    }
+    if (result.length == 1 && result[0] < 0) {
+        return [Math.max(...nums)];
+    }
+    return result;
+};
+
 // @lc code=end
 
-maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+maxSubArray([5,4,-1,7,8])
--- tscode/72.编辑距离.ts
+++ tscode/72.编辑距离.ts
@@ -6,7 +6,7 @@
 
 // @lc code=start
 function minDistance(word1: string, word2: string): number {
-    return soultion_72_2(word1, word2);
+    return soultion_72_3(word1, word2);
 };
 
 function soultion_72_1(word1: string, word2: string): number {
@@ -64,36 +64,36 @@ function soultion_72_2(word1: string, word2: string): number {
 }
 
 function soultion_72_3(word1: string, word2: string): number {
-    // 我们需要明确的是，我们的编辑距离是相对于word1进行的编辑
-    const memoryDict: number[][] = [];
+    const rowLen: number = word1.length + 1;
+    const colLen: number = word2.length + 1;
+    const memo: number[][] = new Array(rowLen);
+    for (let i = 0; i < rowLen; i++) {
+        memo[i] = new Array(colLen).fill(Number.MAX_VALUE);
+        memo[i][0] = i;
+    }
 
-    for (let i = 1; i < word1.length; i++) {
-        for (let j = 1; j < word2.length; j++) {
-            const element = word2[i][j];
+    for (let j = 0; j < colLen; j++) {
+        memo[0][j] = j;
+    }
 
+    for (let i = 1; i < rowLen; i++) {
+        for (let j = 1; j < colLen; j++) {
+            if (word1[i - 1] == word2[j - 1]) {
+                memo[i][j] = memo[i - 1][j - 1];
+            } else {
+                memo[i][j] = Math.min(
+                    memo[i - 1][j], 
+                    memo[i][j - 1], 
+                    memo[i - 1][j - 1]
+                    ) + 1;
+            }
+            
+            
         }
         
     }
-
-    function dp(i:number, j: number): number {
-        if (i == -1) { // 表明word1已经遍历完成
-            return j + 1; // 把word2剩下的所有字符插入到word1
-        }
-        if (j == -1) { // 表明word2已经遍历完成
-            return i + 1; // 把word1剩下的所有字符删除
-        }
-
-        if (word1[i] == word2[j]) {
-            return dp(i - 1, j - 1);
-        } else {
-            return Math.min(
-                dp(i - 1, j), // 删除当前的字符， word1的指针往前移动
-                dp(i, j - 1), // 添加一个字符到word1，word2的指针往前移动
-                dp(i - 1, j - 1) // 替换当前字符，指针都往前移动
-            ) + 1;
-        }
-    }
-    return dp(word1.length - 1, word2.length - 1);
+    return memo[rowLen - 1][colLen - 1];
+ 
 }
 // @lc code=end
 
